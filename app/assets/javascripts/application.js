c7bdeaf139477f6d3f9898c570f90c6b7d7687c7
sakshi-yadav <sakshikumariy@gmail.com>
2020-04-05 00:00:43 +0530

    jquery before bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
+//= require jquery
 //= require bootstrap
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require jquery
 //= require bootstrap-sprockets
 //= require bootstrap-datetimepicker
+//= require activestorage
+//= require turbolinks
 //= require_tree .
