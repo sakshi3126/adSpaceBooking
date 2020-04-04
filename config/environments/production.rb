42951defc58b65ee0365646cf332a08106303a1d
sakshi-yadav <sakshikumariy@gmail.com>
2020-04-05 02:24:41 +0530

    production setting changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,17 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "add-slot-booking.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: "mail.smtp2go.com",
-      port: 2525, # 8025, 587 and 25 can also be used.
-      domain: 'gmail.com',
-      authentication: "plain",
+      address: 'smtp.gmail.com',
+      port: 587, # 8025, 587 and 25 can also be used.
+      domain:               'gmail.com',
+      user_name:            'notify.u.noreply@gmail.com',
+      password:             'Para@0405',
+      authentication:       'plain',
       enable_starttls_auto: true,
-      user_name: 'notify.u.noreply@gmail.com',
-      password: 'Para@0405',
   }
 end
